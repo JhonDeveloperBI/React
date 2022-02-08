3c0876a57ec8adedfe63192419e1eff80071d4cf
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-08 11:00:42 -0500

    Fragment

--- counter-app/src/PrimeraApp.js
+++ counter-app/src/PrimeraApp.js
@@ -1,8 +1,15 @@
 //Functional Component
 
+//import { Fragment } from "react"; // fragment importacion innecesaria
+
 const PrimeraApp =() =>{
 
-    return <h1> Hola Mundo</h1>
+    return ( 
+             <>
+                <h1> Hola Mundo</h1>
+                <p> Mi primera aplicacion</p>
+            </>
+    );
 }
 
 
