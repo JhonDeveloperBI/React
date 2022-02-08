aabd3131d554052a7cb1d78852f6778b61094ffe
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-08 10:47:54 -0500

    Hello World in react

---
+++ counter-app/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDom from "react-dom";
+import PrimeraApp from "./PrimeraApp";
+import './index.css'
+
+
+
+//const saludo = <h1>Hola mundo</h1>;
+
+//console.log(saludo)
+
+const divRoot = document.querySelector('#root');
+
+ReactDom.render(<PrimeraApp />, divRoot);
+
+
