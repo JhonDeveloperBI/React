54db44f1e8f93892faebdd7ef12ba95f4c9e81e0
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-08 11:14:49 -0500

    show objects in react with JSON.Stringify

--- counter-app/src/PrimeraApp.js
+++ counter-app/src/PrimeraApp.js
@@ -3,10 +3,18 @@
 //import { Fragment } from "react"; // fragment importacion innecesaria
 
 const PrimeraApp =() =>{
+    const saludo = 'Hola Mundo';
+   // const saludo =[1,2,3,5,6];
+    // const saludo ={    //fail
+    //     "nombre":"jhon",
+    //     "edad":45
+    // }
+    
 
     return ( 
              <>
-                <h1> Hola Mundo</h1>
+             <h1 > { saludo }</h1>
+                {/* <pre> { JSON.stringify(saludo, null, 3) }</pre> */}
                 <p> Mi primera aplicacion</p>
             </>
     );
