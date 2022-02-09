cbb8a9c87e0d6bcb1528234cd3936a344324469c
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-09 09:35:54 -0500

    handleSubtract and handleReset

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -3,21 +3,29 @@ import PropTypes from "prop-types";
 //Functional Component
 
 
-const CounterApp =( {value} ) =>{ //desestructuracion
+const CounterApp =( {value = 1 } ) =>{ //desestructuracion
 
- const [ counter , setCounter ] = useState(0); //depend setNombre of first argument [ nombre, setNombre]
+ const [ counter , setCounter ] = useState( value ); //depend setNombre of first argument [ nombre, setNombre]
 
 
     const handleAdd = () =>{
-     //setCounter(counter + 1 ) // function for modified the state
-      setCounter ( (c) => c + 1 )
+     setCounter(counter + 1 ) // function for modified the state
     }
+
+    const handleSubtract = () => setCounter(counter - 1)
+    
+
+    const handleReset = () =>  setCounter( value )
+    
+
 
     return ( 
              <>
              <h1>CounterApp</h1>
              <h2> { counter } </h2>
-             <button onClick={  handleAdd }>+1</button>  
+             <button onClick={  handleAdd } > +1 </button>  
+             <button onClick={  handleReset } >Reset</button>
+             <button onClick={  handleSubtract } > -1 </button>
             </>
     );
 }
