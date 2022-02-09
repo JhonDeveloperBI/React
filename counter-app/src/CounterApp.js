26d57b179c0dcf41dba01e9a7ace205f6e8f094d
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-09 09:06:05 -0500

    added Event Click in Component handleAdd

--- counter-app/src/CounterApp.js
+++ counter-app/src/CounterApp.js
@@ -4,10 +4,18 @@ import PropTypes from "prop-types";
 
 const CounterApp =( {value} ) =>{ //desestructuracion
 
+    //handleAAd
+    const handleAdd = (e) =>{
+      console.log(e)    // had a return undefined
+     // return () => console.log('Hola Mundo');
+    }
+
+    // handleAdd() expected return function in this case is hello World
     return ( 
              <>
              <h1>CounterApp</h1>
              <h2> { value } </h2>
+             <button onClick={  handleAdd }>+1</button>  
             </>
     );
 }
