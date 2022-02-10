7e09674d64cdd7b25ecc8b52c6f1c1cd695a8ffa
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-10 13:10:25 -0500

    init enzyme, snapshots test

--- counter-app/src/tests/PrimeraApp.test.js
+++ counter-app/src/tests/PrimeraApp.test.js
@@ -1,13 +1,23 @@
 import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
 import PrimeraApp from "../PrimeraApp";
 
 describe('test in <PrimeraApp />', () => { 
-    test('should show message "Hola', () => { 
+   /*  test('should show message "Hola', () => { 
 
         const saludo = 'Hola';
 
         const { getByText } = render(<PrimeraApp saludo={ saludo } />)
 
         expect( getByText(saludo)).toBeInTheDocument();
-     })
+     }) */
+     
+     test('should be show <PrimeraApp /> success',() => {
+
+        const saludo = 'Hola' 
+        const wrapper = shallow(<PrimeraApp saludo={ saludo }/>) // simulate click, documents
+         
+        expect( wrapper).toMatchSnapshot(); // snapshot
+     });
+
  })
