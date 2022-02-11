b2b9f3007a2e083c27a5fdb4fab2190d6c60c873
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-11 11:55:32 -0500

    GetImages use fetch in component for data in endpoint

---
+++ 06-gif-expert-app/src/components/GifGrid.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+
+const getGifs = async() => {
+
+//    const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=t&limit=10&api_key=A8xMXqzieIHmtO3BjGLAtf1daSSDAv8K`;
+
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+
+    console.log(gifs)
+    //return gifs;
+}
+
+getGifs();
+
+export const GifGrid = ({ category }) => {
+  return (
+    <div>
+        <h3>{ category }</h3>
+    </div>
+  )
+}
