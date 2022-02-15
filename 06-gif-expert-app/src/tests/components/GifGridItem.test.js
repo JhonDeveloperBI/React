c710c45756f8a1690474c1b312b5b10cb722ea22
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-15 12:18:08 -0500

    91. test in component GifGridItem

--- 06-gif-expert-app/src/tests/components/GifGridItem.test.js
+++ 06-gif-expert-app/src/tests/components/GifGridItem.test.js
@@ -5,11 +5,35 @@ describe('test in GifGridItem', () => {
 
     const title = 'test';
     const url = 'https://www.google.com';
+    const wrapper = shallow(<GifGridItem title={title}  url = {url} />)
 
     test('should show <GifGridItem /> correct', () => { 
-        
-        const wrapper = shallow(<GifGridItem title={title}  url = {url} />)
-
         expect( wrapper).toMatchSnapshot(); // snapshot
      })
+
+     test('show had a parraf with the title',() => {
+
+         const p = wrapper.find('p');
+
+         expect(p.text().trim()).toBe( title);
+     })
+
+
+     test('should had the image same url and alt of the props', () => { 
+        
+        const img = wrapper.find('img');
+        
+        expect(img.prop('src') ).toBe( url );
+        expect(img.prop('alt') ).toBe( title );
+        
+      })
+
+      test('should had animate__bounce', () => {
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect( className.includes('animate__bounce') ).toBe( true );
+
+    })
  })
