4c292b8409db08fae9e65402092b971b36ec6b59
Jhon <ramirescortes@hotmail.com>
2022-02-15 19:58:44 -0500

    94. Simulate Onchange in input

--- 06-gif-expert-app/src/tests/components/AddCategory.test.js
+++ 06-gif-expert-app/src/tests/components/AddCategory.test.js
@@ -4,13 +4,24 @@ import { AddCategory } from "../../components/AddCategory"
 describe('Test with <AddCategory /> ', () => {
 
     const setCategories = () => {};
+    const wrapper = shallow(<AddCategory  setCategories={ setCategories }/>);
     
     test('should show correcta', async() => {
-        
-        const wrapper = shallow(<AddCategory  setCategories={ setCategories }/>);
 
         expect(wrapper).toMatchSnapshot();
-        
+
+    })
+
+
+    test('should change the text box',() =>{
+      const input = wrapper.find('input');
+      const value = 'Hola Mundo';
+
+      input.simulate('change',{ target: {
+          value:value
+      }});
+      
+      expect( wrapper.find('p').text().trim() ).toBe(value)
     })
     
 })
