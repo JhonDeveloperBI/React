4129984da91fe6812a839891a3fe61b6f1f9abb6
jhon jaime ramirez cortes <ramirescortes@hotmail.com>
2022-02-17 13:23:37 -0500

    106.use State keep previous state

--- 07-hook-app/src/components/01-useState/CounterApp.js
+++ 07-hook-app/src/components/01-useState/CounterApp.js
@@ -3,23 +3,27 @@ import './counter.css';
 
 export const CounterApp = () => {
 
-    const [{ counter1 , counter2 }, setCounter] = useState({
+    const [state, setState] = useState({
         counter1:10,
-        counter2:20
+        counter2:20,
+        counter3:30,
+        counter4:40,
     })
+
+    const { counter1, counter2 } = state;
 
   return (
       <>
-        <h1>Counter1 {counter1}</h1>
-        <h1>Counter2 {counter2}</h1>
+        <h1>Counter1 { counter1 }</h1>
+        <h1>Counter2 { counter2} </h1>
         
         <hr />
         <button 
             className='btn btn-primary'
             onClick={() => {
-               setCounter({
-                   counter1:counter1 +1,
-                   counter2:counter2 +1
+               setState({
+                   ...state, // keep the previous state 
+                   counter1 : counter1 +1,
                } )
                 
             }
