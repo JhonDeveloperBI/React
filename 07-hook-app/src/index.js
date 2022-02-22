8d739f13ca5a54e99622815696c02c04426dc7c5
Jhon <ramirescortes@hotmail.com>
2022-02-22 08:59:32 -0500

    114. useRef PrimerUso

--- 07-hook-app/src/index.js
+++ 07-hook-app/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
 //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 //import { CounterApp } from './components/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
 //import { SimpleForm } from './components/02-useEffect/SimpleForm';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 
 
 ReactDOM.render(
-    <MultipleCustomHooks />,
+    <FocusScreen />,
   document.getElementById('root')
 );
