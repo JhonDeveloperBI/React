85f7ae3f8dd420f92264187346ddfc8641affad1
Jhon <ramirescortes@hotmail.com>
2022-02-27 14:12:14 -0500

    125. idea general to reduce

--- 07-hook-app/src/index.js
+++ 07-hook-app/src/index.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './components/07-tarea-memo/Padre';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './components/07-tarea-memo/Padre';
 
 
-//import { FocusScreen } from './components/04-useRef/FocusScreen';
-//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-//import { CounterApp } from './components/01-useState/CounterApp';
-//import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-//import { SimpleForm } from './components/02-useEffect/SimpleForm';
-//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-//import { RealExample } from './components/04-useRef/RealExample';
-//import { Layout } from './components/05-useLayoutEffect/Layout';
-//import { Memorize } from './components/06-memos/Memorize';
-//import { MemoHook } from './components/06-memos/MemoHook';
-//import { CallbackHook } from './components/06-memos/CallbackHook';
+// //import { FocusScreen } from './components/04-useRef/FocusScreen';
+// //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// //import { CounterApp } from './components/01-useState/CounterApp';
+// //import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// //import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// //import { RealExample } from './components/04-useRef/RealExample';
+// //import { Layout } from './components/05-useLayoutEffect/Layout';
+// //import { Memorize } from './components/06-memos/Memorize';
+// //import { MemoHook } from './components/06-memos/MemoHook';
+// //import { CallbackHook } from './components/06-memos/CallbackHook';
 
-ReactDOM.render(
-    <Padre />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Padre />,
+//   document.getElementById('root')
+// );
+import './components/08-useReducer/intro-reducer'
