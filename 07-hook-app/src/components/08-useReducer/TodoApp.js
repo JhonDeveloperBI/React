556bcb67176ad4e3cc0ce4279c7411cd5fd49aeb
Jhon <ramirescortes@hotmail.com>
2022-02-27 15:02:25 -0500

    127 part two

--- 07-hook-app/src/components/08-useReducer/TodoApp.js
+++ 07-hook-app/src/components/08-useReducer/TodoApp.js
@@ -18,14 +18,49 @@ export const TodoApp = () => {
 
   return (
     <div>
-        <h1>Todo App</h1>
+        <h1>Todo App ({todos.length})</h1>
         <hr />
 
-        <ul>
-            <li>Hello</li>
-            <li>World</li>
-            <li>Hola of new</li>
-        </ul>
+     <div className="row">
+         <div className="col-7">
+                <ul className="list-group list-group-flush">
+                {
+                    todos.map( (todo, i) => {
+                        <li key ={ todo.id }
+                            className="list-group-item">
+                                <p className="text-center ">{ i +1}.  { todo.desc } </p>
+                                <button 
+                                    className="btn btn-danger"> Borrar </button> 
+                            </li>
+                    })
+                }
+                </ul>
+         </div>
+
+         <div className="col-5">
+             <h4> Agregar TODO</h4>
+             <hr />
+
+             <form>
+                 <input
+                      type="text"
+                      name="description"
+                      className="form-control"
+                      placeholder="Aprender ..."
+                      autoComplete="off"
+                      />
+
+                <button
+                  className="btn btn-outline-primary mt-1 btn-block"
+                >
+                 Agregar
+                </button>
+                
+
+             </form>
+
+         </div>
+     </div>
     </div>
   )
 }
