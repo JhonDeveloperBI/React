8b662f339d7d11220d89a3f211965bc7f1c77c76
Jhon <ramirescortes@hotmail.com>
2022-02-28 18:01:49 -0500

    140. Link and NavLink

--- 07-hook-app/src/components/09-useContext/AppRouter.js
+++ 07-hook-app/src/components/09-useContext/AppRouter.js
@@ -3,18 +3,24 @@ import {
     Routes,
     Route
   } from "react-router-dom";
+import { NavBar } from "./NavBar";
+
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from "./LoginScreen";
 import { HomeScreen } from "./HomeScreen";
 
 
+
 export const AppRouter = () => {
   return (
     <Router>
+      <NavBar />
         <Routes>
                         <Route  path="/" element={ <HomeScreen /> } />
                         <Route  path="/about" element={ <AboutScreen />} />
                         <Route  path="/login" element={ <LoginScreen /> } />
+
+                        <Route path="*" element={ <HomeScreen />} /> 
         </Routes>
     </Router>
   )
