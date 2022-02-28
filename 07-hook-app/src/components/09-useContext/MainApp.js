0a7f89590239f2a1f3efed59a28a21b928899aaa
Jhon <ramirescortes@hotmail.com>
2022-02-28 18:28:30 -0500

    141. CreateContext and UseContext

--- 07-hook-app/src/components/09-useContext/MainApp.js
+++ 07-hook-app/src/components/09-useContext/MainApp.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
+  const user = {
+    id:1234,
+    name: 'jhon',
+    email:'jhonsito'
+  }
+
   return (
+    <UserContext.Provider value={user }>
         <AppRouter />
+    </UserContext.Provider>    
   )
 }
