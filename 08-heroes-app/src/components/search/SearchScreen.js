cbdd3a4b0f45418587b0a6c5a61f7baebe641c67
Jhon <ramirescortes@hotmail.com>
2022-03-02 09:27:45 -0500

    182. SearchComponent Use customHook useForm

--- 08-heroes-app/src/components/search/SearchScreen.js
+++ 08-heroes-app/src/components/search/SearchScreen.js
@@ -1,9 +1,50 @@
+import { useForm } from "../../hooks/useForm";
 
 export const SearchScreen = () => {
+
+  const initialForm = {
+    searchText: ''
+};
+
+const [ formValues, handleInputChange ] = useForm( initialForm );
+
+const { searchText } = formValues;
+
+  const handleSearch = ( e ) =>{
+    e.preventDefault();
+
+    console.log(searchText)
+
+  }
+
     return (
-      <div>
-          <h1>SearchScreen</h1>
-      </div>
+      <>
+          <h1>Busquedas</h1>
+          <hr />
+
+          <div className="row">
+            <div className="col-5">
+              <h4> Buscar</h4>
+              <hr />
+
+              <form onSubmit={ handleSearch }>
+                 <input
+                    type="text"
+                    placelholder =" Buscar un heroe"
+                    className= "form-control"
+                    name="searchText"
+                    autoComplete="off"
+                    value={ searchText }
+                    onChange={ handleInputChange }
+                    />
+
+                    <button  className="btn btn-outline-primary mt-1 " type="submit">
+                        Buscar...
+                    </button>
+              </form>
+            </div>
+          </div>
+      </>
     )
   }
   
