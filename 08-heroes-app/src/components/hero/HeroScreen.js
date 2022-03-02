1c2ffaf1f9683603ccdbde2c4eef582fae5c4f96
Jhon <ramirescortes@hotmail.com>
2022-03-02 08:08:59 -0500

    178.read argument for URL

--- 08-heroes-app/src/components/hero/HeroScreen.js
+++ 08-heroes-app/src/components/hero/HeroScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import { useParams, Navigate } from "react-router-dom"
+import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
+
+  const { heroeId } = useParams(); // get data of the URl
+  const hero = getHeroById( heroeId);
+
+ // if( !hero ) return <p> No hay heroe</p>
+
+ if(!hero){
+    return <Navigate to='/' />
+ }
+
   return (
     <div>
         <h1>HeroScreen</h1>
+        <p>{ hero.superhero }</p>
     </div>
   )
 }
