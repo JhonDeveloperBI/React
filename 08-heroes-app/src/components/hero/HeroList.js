800d988a449375c06ee07719750a8ed1b207bb27
Jhon <ramirescortes@hotmail.com>
2022-03-02 08:45:34 -0500

    180 Use Memo Hook

--- 08-heroes-app/src/components/hero/HeroList.js
+++ 08-heroes-app/src/components/hero/HeroList.js
@@ -1,10 +1,13 @@
+import { useMemo } from 'react';
+
 import { HeroCard } from './HeroCard';
 import { getHeroesByPublish } from '../../selectors/getHeroesByPublisher'
 
 
+
 export const HeroList = ({ publisher }) => {
 
-    const heroes = getHeroesByPublish( publisher );
+    const heroes = useMemo (() => getHeroesByPublish( publisher ) ,[ publisher ]);
   return (
     <div className="row rows-cols-1 row-cols-md-3 g-3"> 
             <li>
--- 08-heroes-app/src/components/hero/HeroScreen.js
+++ 08-heroes-app/src/components/hero/HeroScreen.js
@@ -1,10 +1,11 @@
+import { useMemo } from "react";
 import { useParams, Navigate, useNavigate } from "react-router-dom"
 import { getHeroById } from "../../selectors/getHeroById";
 
 export const HeroScreen = () => {
 
   const { heroeId } = useParams(); // get data of the URl
-  const hero = getHeroById( heroeId);
+  const hero = useMemo( () => getHeroById( heroeId) ,[ heroeId ] );
   const navigate = useNavigate();
 
   const handleReturn = () => {
