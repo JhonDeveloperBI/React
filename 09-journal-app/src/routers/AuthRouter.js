616b7f144e6980cab1f3b3e8a6d2127859597d0c
Jhon <ramirescortes@hotmail.com>
2022-03-04 16:33:04 -0500

    227. implement rutes daughter and main

---
+++ 09-journal-app/src/routers/AuthRouter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Route } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import { LoginScreen } from '../components/auth/LoginScreen'
+import { RegisterScreen } from '../components/auth/RegisterScreen'
+
+export const AuthRouter = () => {
+  return (
+    <div>
+        <Switch>
+            <Route exact path="/auth/login" component={ LoginScreen } ></Route>
+            <Route exact path="/auth/register" component={ RegisterScreen } ></Route>
+            
+            <Redirect to="/auth/login" />
+            
+        </Switch>
+    </div>
+  )
+}
