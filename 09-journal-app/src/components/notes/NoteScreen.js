f8a7ab35270ff3f204562761553dc4e6d0cce515
Jhon <ramirescortes@hotmail.com>
2022-03-07 09:17:50 -0500

    236. structured and design to component HomeScreen

--- 09-journal-app/src/components/notes/NoteScreen.js
+++ 09-journal-app/src/components/notes/NoteScreen.js
@@ -2,9 +2,35 @@ import React from 'react'
 import { NotesAppBar } from './NotesAppBar'
 
 export const NoteScreen = () => {
-  return (
-    <div className="notes_main-content">
-        <NotesAppBar />
-    </div>
-  )
+    return (
+        <div className="notes__main-content">
+            
+            <NotesAppBar />
+
+            <div className="notes__content">
+
+                <input 
+                    type="text"
+                    placeholder="Some awesome title"
+                    className="notes__title-input"
+                    autoComplete="off"
+                />
+
+                <textarea
+                    placeholder="What happened today"
+                    className="notes__textarea"
+                ></textarea>
+
+                <div className="notes__image">
+                    <img 
+                        src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg"
+                        alt="imagen"
+                    />
+                </div>
+
+
+            </div>
+
+        </div>
+    )
 }
