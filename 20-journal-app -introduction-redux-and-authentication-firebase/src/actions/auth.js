387882ba1721baceaf6fdee86087224f1cd72971
Jhon <ramirescortes@hotmail.com>
2022-03-07 14:32:24 -0500

    248. thunk Middleware- Actions  asyn
    install redux-thunk applied Middleware

--- 20-journal-app -introduction-redux-and-authentication-firebase/src/actions/auth.js
+++ 20-journal-app -introduction-redux-and-authentication-firebase/src/actions/auth.js
@@ -1,5 +1,14 @@
 import { types } from "../components/types/types"
 
+export const startLoginEmailPassword = ( email,password) =>{
+    return ( dispatch) =>{
+
+        setTimeout( ()=>{
+          dispatch( login(123,'Pedro'))
+        },3500)
+    }
+}
+
 export const login = ( uid , displayName) =>{
    return {
        type: types.login,
