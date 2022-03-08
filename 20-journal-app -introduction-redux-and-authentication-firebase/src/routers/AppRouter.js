d82c5ea21ccf32d2fd81908497dadca8f8bb7ba8
Jhon <ramirescortes@hotmail.com>
2022-03-08 11:35:09 -0500

    257. keep state user in Application with UserEffect

--- 20-journal-app -introduction-redux-and-authentication-firebase/src/routers/AppRouter.js
+++ 20-journal-app -introduction-redux-and-authentication-firebase/src/routers/AppRouter.js
@@ -1,22 +1,31 @@
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import {
     BrowserRouter as Router,
     Switch,
     Route
 } from 'react-router-dom';
+import { login } from '../actions/auth';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { projectAuth } from '../firebase/fireabase-config';
 import { AuthRouter } from './AuthRouter';
 
 export const AppRouter = () => {
 
+   const dispatch = useDispatch();
+
   useEffect(() => {
       projectAuth.onAuthStateChanged( ( user ) =>{
         
-        console.log(user);
-      })
-  }, [])
+        //console.log(user);
+         if( user?.uid ){
+           dispatch( login(user.uid, user.displayName) ) 
+         }
+
+      });
+
+  }, [ dispatch ])  // only execute one 
   
   return (
     <Router>
