c611659ef643c56dddfc6c35755f844f85746ed2
Jhon <ramirescortes@hotmail.com>
2022-03-08 10:56:21 -0500

    255. login with email and password validated form

--- 20-journal-app -introduction-redux-and-authentication-firebase/src/components/auth/LoginScreen.js
+++ 20-journal-app -introduction-redux-and-authentication-firebase/src/components/auth/LoginScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom'
 import {  startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../hooks/useForm';
+import isEmail from 'validator/lib/isEmail';
+import { removeError, setError } from '../../actions/ui';
 
 export const LoginScreen = () => {
 
   const dispatch =useDispatch();
+  const { msgError } = useSelector(state => state.ui );
 
   const [formValues, handleInputChange] = useForm({
     email:'test@gmail.com',
@@ -17,21 +20,48 @@ const { email, password} = formValues;
 
 const handleLogin = (e) =>{
    e.preventDefault();
-   console.log(email,password);
-
-   dispatch( startLoginEmailPassword(email, password) );
-    
+  
+   if( isFormValid() ){
+        dispatch( startLoginEmailPassword(email, password) );
+   } 
 }
 
 const handleGoogleLogin = () =>{
      dispatch( startGoogleLogin())
 }
 
+
+const isFormValid = () => {
+  
+    if( password.trim().length === 0){
+        dispatch( setError(' the password is required') )
+       // console.log(' the name is required')
+        return false;
+
+    }else if( ! isEmail( email) ){ 
+      // console.log(' the email is invalid')
+        dispatch( setError(' the email is invalid ') )
+      
+        return false;
+   }
+   
+   dispatch( removeError() )
+    return true;
+}
+
     return (
         <>
             <h3 className="auth__title">Login</h3>
-
+        
             <form>
+
+            {
+                    msgError &&
+                    (  <div className="auth__alert-error">
+                        { msgError }
+                        </div>
+                    )
+                }
 
                 <input 
                     type="text"
