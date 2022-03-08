3febd111fabc55c674c18a7777ee49ca0fba8798
Jhon <ramirescortes@hotmail.com>
2022-03-08 15:59:10 -0500

    269. active note created

--- 21-journal-app-introduction-redux-crud-firestore-upload-file/src/actions/notes.js
+++ 21-journal-app-introduction-redux-crud-firestore-upload-file/src/actions/notes.js
@@ -19,13 +19,14 @@ export const startNewNote = () => {
 
         const doc = await db.collection(`${ uid }/journal/notes`).add( newNote );
 
-        console.log( doc )
-       // dispatch( activeNote( doc.id, newNote ) );
-       // dispatch( addNewNote( doc.id, newNote ) );
+       
+        dispatch( activeNote( doc.id, newNote ) );
+        //dispatch( addNewNote( doc.id, newNote ) );
 
     }
 }
 
+// active Note
 export const activeNote = ( id, note ) => ({
     type: types.notesActive,
     payload: {
@@ -34,13 +35,16 @@ export const activeNote = ( id, note ) => ({
     }
 });
 
+
+// add NewNote
+/*
 export const addNewNote = ( id, note ) => ({
     type: types.notesAddNew,
     payload: {
         id, ...note
     }
 })
-
+*/
 /*
 export const startLoadingNotes = ( uid ) => {
     return async( dispatch ) => {
