7199683bcb54a1dbed90f1c19d940d1259809bb3
Jhon <ramirescortes@hotmail.com>
2022-03-24 11:29:30 -0500

    313. configurations calendars

--- 22-calendar-app/src/components/calendar/CalendarScreen.js
+++ 22-calendar-app/src/components/calendar/CalendarScreen.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { Calendar, momentLocalizer } from 'react-big-calendar'
-import moment from 'moment'
+import moment from 'moment';
+import 'moment/locale/es';
 
 import { Navbar } from '../ui/Navbar'
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { messages } from '../../helpers/calendar-messages-es';
+
+moment.locale('es');
 
 const localizer = momentLocalizer(moment);
 
@@ -11,10 +15,25 @@ const events = [{
   title: 'Cumpleaños',
   start: moment().toDate(),
   end: moment().add(2,'hours').toDate(),
-  bgcolor: '#fafafa'
+  bgcolor: '#fafafa',
+  notes: 'Buy ticket'
 }]
 
 export const CalendarScreen = () => {
+
+  const eventStyleGetter = ( event, start, end, isSelected) => {
+    const style ={
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity:0.8,
+      display:'block',
+      color:'white'
+    }
+
+    return {
+      style
+    }
+  }
   return (
     <div className="calendar-screen">
         <Navbar />
@@ -24,6 +43,8 @@ export const CalendarScreen = () => {
             events={ events }
             startAccessor="start"
             endAccessor="end"
+            messages={ messages }
+            eventPropGetter={ eventStyleGetter }
             />
     </div>
   )
