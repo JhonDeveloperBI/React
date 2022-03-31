f64d4c7195aacf1d0453a35702365491531c910c
Jhon <ramirescortes@hotmail.com>
2022-03-31 12:26:30 -0500

    331. configurated Express

--- 23-calendar-backend/index.js
+++ 23-calendar-backend/index.js
@@ -1,1 +1,13 @@
-console.log("hola mundo");
+const express = require('express');
+// create a new express app
+const app = express();
+
+// route    
+app.get('/', function(req, res){
+    res.send("Hello World");
+});
+
+//listen for requests
+app.listen(4000, function(){
+    console.log("Server is listening on port 4000");
+});
