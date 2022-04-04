281acdd863c57c0282f69e9cc12d0a1b57da6491
Jhon <ramirescortes@hotmail.com>
2022-04-04 18:25:04 -0500

    352. Model Event

--- 23-calendar-backend/controllers/events.js
+++ 23-calendar-backend/controllers/events.js
@@ -11,6 +11,18 @@ const getEventos = (req, res = response) => {
 
 
 const crearEvento = (req, res = response) => {
+
+    //console.log(req.body); 
+    /*
+      {
+        "title":"Evento 1",
+        "start":0,
+         "end":0
+      }
+    */
+
+
+
     res.status(200).json({
         ok: true,
         msg:'crearEvento'
