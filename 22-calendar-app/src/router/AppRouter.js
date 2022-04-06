0280e8fa23840a3658f8cc31e4380a2010da1af5
Jhon <ramirescortes@hotmail.com>
2022-04-06 18:16:16 -0500

    373. protected ours rutes

--- 22-calendar-app/src/router/AppRouter.js
+++ 22-calendar-app/src/router/AppRouter.js
@@ -1,31 +1,55 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom";
-import { startChecking } from '../actions/auth';
+    BrowserRouter as Router,
+    Switch,
+    Redirect
+  } from 'react-router-dom';
+  
+import { useDispatch, useSelector } from 'react-redux';
+
 import { LoginScreen } from '../components/auth/LoginScreen';
 import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { startChecking } from '../actions/auth';
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
 
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    dispatch( startChecking())
-  }, [dispatch])
-  
-  
-  return (
-    <BrowserRouter>
-     <Routes>
-        <Route path="/login" element={<LoginScreen />} />
-        <Route path="/" element={<CalendarScreen />} />
+    const dispatch = useDispatch();
+    const { checking, uid } = useSelector( state => state.auth);
+
+    useEffect(() => {
         
-        <Route path="*" element={<LoginScreen />} />
-    </Routes> 
-    </BrowserRouter>
-  )
+        dispatch( startChecking() );
+
+    }, [dispatch])
+
+    if ( checking ) {
+        return (<h5>Espere...</h5>);
+    }
+
+    return (
+        <Router>
+            <div>
+                <Switch>
+
+                    <PublicRoute 
+                        exact 
+                        path="/login" 
+                        component={ LoginScreen }
+                        isAuthenticated={ !!uid }
+                    />
+
+                    <PrivateRoute 
+                        exact 
+                        path="/" 
+                        component={ CalendarScreen } 
+                        isAuthenticated={ !!uid }
+                    />
+
+                    <Redirect to="/" />   
+                </Switch>
+            </div>
+        </Router>
+    )
 }
